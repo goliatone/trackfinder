20820ff892ffa5e5742adb4d270b307b33061e02
goliatone <burgosemi@gmail.com>
2014-08-06 07:25:08 -0400

    added index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * tuxedo
+ * https://github.com/goliatone/tuxedo
+ *
+ * Copyright (c) 2014 goliatone
+ * Licensed under the MIT license.
+ */
+'use strict';
+
+module.exports = require('./lib/trackfinder');
