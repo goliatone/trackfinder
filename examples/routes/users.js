3c3fb397a1fcb0b856955d3fdfdf177ecd8d4f63
goliatone <burgosemi@gmail.com>
2017-04-30 16:41:15 -0400

    Update example users route

--- examples/routes/users.js
+++ examples/routes/users.js
@@ -1,27 +1,18 @@
+'use strict';
+
 var Route = {};
 
-Route.priority = 10;
-Route.using = '/api';
+Route.priority = 1;
 
 Route.register = function(app, options){
     console.log('- users: register routes');
-    app.get('/peperone', function(req, res){
-        res.send('Peperone is here, mofos!');
+    app.get('/me', function(req, res){
+        res.status(200).json({
+            id: 1,
+            name: 'peperone',
+            email: 'pepe@rone.com'
+        });
     });
-};
-
-Route.routes = {
-    'GET /users': function(req, res) {
-        res.send('==> GET users');
-    },
-    'GET /:resource/:id': function(req, res) {
-        console.log('params', req.params);
-        res.send('===> GET user: '+ req.params.resource+ ' '+ req.params.id);
-    },
-    'POST /:resource': function(req, res) {},
-    'PUT /:resource/:id': function(req, res) {},
-    'PATCH /:resource/:id': function(req, res) {},
-    'DELETE /:resource/:id': function(req, res) {}
 };
 
 module.exports = Route;
