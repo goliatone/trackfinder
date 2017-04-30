a5b2e371b471f0b8423c9fedc3dc191f42c389b2
goliatone <burgosemi@gmail.com>
2017-04-30 17:36:44 -0400

    Refactor examples to use test

--- examples/routes/users.js
+++ examples/routes/users.js
@@ -1,18 +1,22 @@
 'use strict';
 
-var Route = {};
+let Route = {};
 
 Route.priority = 1;
 
-Route.register = function(app, options){
-    console.log('- users: register routes');
+let response = {
+    id: 1,
+    name: 'peperone',
+    email: 'pepe@rone.com'
+};
+
+Route.register = function(app, options) {
+    options.logger.info('- users: register routes');
+
     app.get('/me', function(req, res){
-        res.status(200).json({
-            id: 1,
-            name: 'peperone',
-            email: 'pepe@rone.com'
-        });
+        res.status(200).json(response);
     });
 };
 
 module.exports = Route;
+module.exports.response = response;
