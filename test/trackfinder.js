edf4867271edd4c9a630f2af430ded4a49d90785
goliatone <burgosemi@gmail.com>
2017-04-30 19:02:46 -0400

    Update trackfinder test

--- test/trackfinder.js
+++ test/trackfinder.js
@@ -1,55 +1,17 @@
 'use strict';
 const test = require('tape');
 const resolve = require('path').resolve;
-const TrackFinder = require('../lib');
+const TrackFinder = require('..').TrackFinder;
 
-test('TrackFinder has DEFAULTS', (t) => {
-    t.ok(TrackFinder.constructor.DEFAULTS, 'OK defaults.');
-    t.end();
-});
+test('TrackFinder addFileFilter should be stored in filters', (t) => {
 
-test('TrackFinder setOptions should merge DEFAULTS', (t)=>{
-
-    let DEFAULTS = TrackFinder.constructor.DEFAULTS;
-    let options = TrackFinder.setOptions();
-
-    Object.keys(DEFAULTS).map((key)=>{
-        t.deepEquals(options[key], DEFAULTS[key], `${key} should be equal`);
-    });
-
-    t.end();
-});
-
-test('TrackFinder addFileFilter should be stored in filters', (t)=>{
+    let tracker = new TrackFinder();
     let test = function(){};
-    let expected = TrackFinder.filters.concat([test]);
-    TrackFinder.addFileFilter(test);
-    t.deepEquals(TrackFinder.filters, expected);
+
+    let expected = tracker.filters.concat([test]);
+    tracker.addFileFilter(test);
+    t.deepEquals(tracker.filters, expected);
     t.end();
 });
 
-test('TrackFinder filterFile should return true if a filter applies to ar filename', (t)=> {
-    //By default we ignore index.js files
-    t.ok(TrackFinder.filterFile('index.js'));
-    t.end();
-});
-
-test('TrackFinder should find route files in a directory', (t) => {
-    let files = TrackFinder.find(('./test/fixtures/routes'));
-    t.equals(files.length, 2, 'Should find files');
-    t.end();
-});
-
-test('TrackFinder should register route files in a directory', (t) => {
-    let app = {
-        use: function(){}
-    };
-
-    let routers = TrackFinder.register(app, {
-        path: './test/fixtures/routes',
-        config: {}
-    });
-
-    t.equals(routers.length, 2, 'Should return a router per route file');
-    t.end();
-});
+test('')
