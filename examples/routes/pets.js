c20e080af6e8c78abf667ee059e4aa9216a13b32
goliatone <burgosemi@gmail.com>
2017-04-30 16:41:06 -0400

    Update example pets route

--- examples/routes/pets.js
+++ examples/routes/pets.js
@@ -3,9 +3,25 @@
 module.exports = function(app, options){
     console.log('- pets: register routes');
 
-    app.get('/pets', function(req, res){
+    /*
+     * We can override /api routes.
+     *
+     * We need to:
+     * - load this route before API.
+     * - provide the full route
+     *
+     * that is, not use the mountpath.
+     */
+    app.get('/api/pets', function(req, res){
         res.json({
-            pets:['Colonel Meow','Choupette', 'Casper', 'Chase No Face', 'Cherry Pop', 'Blackie the Talking Cat']
+            pets: [
+                'Colonel Meow',
+                'Choupette',
+                'Casper',
+                'Chase No Face',
+                'Cherry Pop',
+                'Merry the Talking Cat'
+            ]
         });
     });
 };
