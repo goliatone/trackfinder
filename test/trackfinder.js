e98f0598a1a53e51cd78c75ec33103d0ca0fe637
goliatone <burgosemi@gmail.com>
2017-04-30 15:58:34 -0400

    Update trackfinder test

---
+++ test/trackfinder.js
@@ -0,0 +1,46 @@
+'use strict';
+const test = require('tape');
+const resolve = require('path').resolve;
+const TrackFinder = require('../lib/trackfinder');
+
+test('TrackFinder has DEFAULTS', (t) => {
+    t.ok(TrackFinder.constructor.DEFAULTS, 'OK defaults.');
+    t.end();
+});
+
+test('TrackFinder setOptions should merge DEFAULTS', (t)=>{
+    let options = {};
+    let DEFAULTS = TrackFinder.constructor.DEFAULTS;
+    TrackFinder.setOptions(options);
+
+    Object.keys(DEFAULTS).map((key)=>{
+        t.deepEquals(TrackFinder[key], DEFAULTS[key], `${key} should be equal`);
+    });
+    t.end();
+});
+
+test('TrackFinder addFileFilter should be stored in filters', (t)=>{
+    let test = function(){};
+    let expected = TrackFinder.filters.concat([test]);
+    TrackFinder.addFileFilter(test);
+    t.deepEquals(TrackFinder.filters, expected);
+    t.end();
+});
+
+test('TrackFinder filterFile should return true if a filter applies to ar filename', (t)=> {
+    //By default we ignore index.js files
+    t.ok(TrackFinder.filterFile('index.js'));
+    t.end();
+});
+
+test('TrackFinder should find route files in a directory', (t) => {
+    let files = TrackFinder.find(('./test/fixtures/routes'));
+    t.equals(files.length, 2, 'Should find files');
+    t.end();
+});
+
+test('', (t) => {
+    let files = TrackFinder.find(('./test/fixtures/routes'));
+    t.equals(files.length, 2, 'Should find files');
+    t.end();
+});
