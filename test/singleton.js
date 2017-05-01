1a2d4fa252c438dfae415c1e02d940013cac5fe4
goliatone <burgosemi@gmail.com>
2017-04-30 21:10:15 -0400

    Test for singleton

--- test/singleton.js
+++ test/singleton.js
@@ -3,36 +3,14 @@ const test = require('tape');
 const resolve = require('path').resolve;
 const TrackFinder = require('..');
 
-test('TrackFinder addFileFilter should be stored in filters', (t)=>{
-    let test = function(){};
-    let expected = TrackFinder.filters.concat([test]);
-    TrackFinder.addFileFilter(test);
-    t.deepEquals(TrackFinder.filters, expected);
-    t.end();
-});
 
-test('TrackFinder normalizePath should return a path by default', (t)=>{
-    t.ok(TrackFinder.normalizePath(), 'should return a path');
-    t.end();
-});
 
-test('TrackFinder should resolve non absolute paths', (t)=>{
-    TrackFinder.basepath = __dirname;
-    let expected = __dirname;
-    let normalized = TrackFinder.normalizePath('./fixtures');
-    t.same(normalized, expected);
-    t.end();
-});
-
-test('TrackFinder filterFile should return true if a filter applies to ar filename', (t)=> {
-    //By default we ignore index.js files
-    t.ok(TrackFinder.filterFile('index.js'));
-    t.end();
-});
 
 test.only('TrackFinder should find route files in a directory', (t) => {
     TrackFinder.setOptions();
-    let files = TrackFinder.find(('./test/fixtures/routes'));
+    TrackFinder.basepath = __dirname;
+    let routes = TrackFinder.normalizePath('./fixtures/routes');
+    let files = TrackFinder.find(routes);
     t.equals(files.length, 2, 'Should find files');
     t.end();
 });
